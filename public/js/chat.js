456495698b54f964376f1208a2fe564547468c00
Luis Mariano Lopez Lena Valdivieso <luismllv89@gmail.com>
2023-08-02 22:26:02 -0600

    validar JWT

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,58 @@
 
-const socket = io();
+const url = 'http://localhost:8080/api/auth/';
+
+let usuario = null;
+let socket = null;
+
+//Validar el token del local storage
+const validarJWT = async() => {
+
+    const token = localStorage.getItem('token') || '';
+
+    if(token.length <= 10){
+        window.location = 'index.html';
+        throw new Error('No hay token en el servidor');
+    }
+
+    const resp = await fetch( url, {
+        headers:{'x-token':token}
+    });
+
+    const { usuario:userDB, token:tokenDB } = await resp.json();
+    localStorage.setItem( 'token',tokenDB );
+    usuario = userDB;
+    document.title = usuario.nombre; 
+
+    await conectarSocket();
+}
+
+const conectarSocket = async() => {
+
+    const socket = io({
+        'extraHeaders':{
+            'x-token':localStorage.getItem('token')
+        }
+
+
+    });
+
+}
+
+const main = async() => {
+
+    await validarJWT();
+
+
+
+}
+
+
+
+
+main();
+
+
+// const socket = io();
 
 
 
